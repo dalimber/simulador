dda11d7df4b591f0dc7e78817011ef01e10fec4a
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-07 13:46:17 -0500

    Agreagr funcion para calcular disponible mediante boton

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos,egresos) 
+{
+    let disponible=ingresos-egresos;
+    if (disponible>0) 
+        {return disponible;} 
+    else { return 0;}
+    
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() 
+{
+    let cmpIngresos=document.getElementById("txtIngresos");
+    let cmpEgresos=document.getElementById("txtEgresos");
+    let ingresos=parseFloat(cmpIngresos.value);
+    let egresos=parseFloat(cmpEgresos.value);
+
+    let valorDisponible=calcularDisponible(ingresos,egresos);
+    frmtValorDisponible=valorDisponible.toFixed(2);
+    
+
+    //MOSTRAR EN PANTALLA
+    let cmpLblDisponible=document.getElementById("spnDisponible");
+    cmpLblDisponible.textContent=frmtValorDisponible;
+    
+}
